b1eb04d814ff42e4c87fda10ed8442b4c61c96bf
Kaushik <kaushikhareesh@gmail.com>
2021-03-04 18:30:53 +0530

    m

--- certificate/serializers.py
+++ certificate/serializers.py
@@ -11,4 +11,4 @@ class GetCertificateSerializer(serializers.ModelSerializer):
     user=UserSerializer()
     class Meta:
         model=Certificates
-        fields=('user','csp','level','certname','certid','certified_date','expiry_date','pdf_url','sbu')
+        fields=('id','user','csp','level','certname','certid','certified_date','expiry_date','pdf_url','sbu')
