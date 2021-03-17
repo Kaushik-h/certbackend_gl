f5ecb95e1ae3e4c615ba27c4d634ae913e448efa
Kaushik <kaushikhareesh@gmail.com>
2021-03-17 15:23:44 +0530

    m

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .views import *
 
-# Create your tests here.
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        response = self.client.get('/getquiz/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
